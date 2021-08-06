c557428e1233d0a5cf3c5d43e63678a013f0ea50
deltegui <deltegui@hotmail.com>
2021-08-06 19:06:46 +0200

    update jwt dependency

--- back/go.mod
+++ back/go.mod
@@ -5,11 +5,12 @@ go 1.15
 require (
 	github.com/deltegui/configloader v1.2.1
 	github.com/deltegui/phoenix v0.17.4
-	github.com/dgrijalva/jwt-go v1.0.2
 	github.com/go-chi/chi v1.5.1
-	github.com/lib/pq v1.10.2
 	github.com/go-playground/validator/v10 v10.4.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.10.2
 	github.com/robfig/cron/v3 v3.0.1
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- back/token/jwt.go
+++ back/token/jwt.go
@@ -6,7 +6,7 @@ import (
 	"temsys"
 	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/golang-jwt/jwt"
 )
 
 // Tokenizer is an implementation of a user Tokenizer using JSON Web Tokens.
@@ -23,10 +23,12 @@ func New(signKey string) Tokenizer {
 func (t Tokenizer) Tokenize(user temsys.User) temsys.Token {
 	expiration := time.Now().Add(time.Hour * 5)
 	token := jwt.New(jwt.GetSigningMethod("HS256"))
-	token.Claims["owner"] = user.Name
-	token.Claims["expires"] = expiration.Unix()
-	token.Claims["role"] = user.Role
-	token.Header["alg"] = "HS256"
+	token.Claims = jwt.MapClaims(map[string]interface{}{
+		"owner":   user.Name,
+		"expires": expiration.Unix(),
+		"role":    user.Role,
+		"alg":     "HS256",
+	})
 	tokenStr, err := token.SignedString([]byte(t.signKey))
 	if err != nil {
 		log.Printf("Something wrong occurred while signing a JWT token for user: '%s': %s\n", user.Name, err)
@@ -43,20 +45,21 @@ func (t Tokenizer) Tokenize(user temsys.User) temsys.Token {
 // Decode raw JWT token and stores the result into a Token struct.
 // It can return an error if something wrong happens when decoding.
 func (t Tokenizer) Decode(raw string) (temsys.Token, error) {
-	token, err := jwt.Parse(raw, func(token *jwt.Token) ([]byte, error) {
+	token, err := jwt.Parse(raw, func(token *jwt.Token) (interface{}, error) {
 		if token.Header["alg"] != "HS256" {
-			return nil, fmt.Errorf("Unknown alg")
+			return nil, fmt.Errorf("unknown algorithm used in JWT")
 		}
 		return []byte(t.signKey), nil
 	})
 	if err != nil || !token.Valid {
 		return temsys.Token{}, err
 	}
-	roleRaw := token.Claims["role"].(string)
+	claims := token.Claims.(jwt.MapClaims)
+	roleRaw := claims["role"].(string)
 	role := temsys.Role(roleRaw)
 	return temsys.Token{
-		Expires: time.Unix(int64(token.Claims["expires"].(float64)), 0),
-		Owner:   token.Claims["owner"].(string),
+		Expires: time.Unix(int64(claims["expires"].(float64)), 0),
+		Owner:   claims["owner"].(string),
 		Value:   raw,
 		Role:    role,
 	}, nil
